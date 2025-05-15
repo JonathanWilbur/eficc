e731116ca2eff0cade53e76e6670d7d9438b89d1
Jonathan M. Wilbur <jonathan@wilbur.space>
2025-05-15 06:54:52 -0400

    chore: move license out of header comment

--- eficc.c
+++ eficc.c
@@ -1,32 +1,6 @@
-/*
-Based off of: https://github.com/rui314/chibicc/tree/main
-Modified by Jonathan M. Wilbur <jonathan@wilbur.space>.
-
-License at the time of copying:
-
-MIT License
-
-Copyright (c) 2019 Rui Ueyama
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-*/
+/* Based off of: https://github.com/rui314/chibicc/tree/main
+License at the time of copying can be found in lic/chibicc.license.txt
+Modified by Jonathan M. Wilbur <jonathan@wilbur.space>. */
 
 // TODO: The rest of these headers can only be eliminated once you transition this to work on EFI only.
 #ifndef NOLIBC
